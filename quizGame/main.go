31766a821be42224fbc6024eb4b76d28c0b6b94e
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-09-26 16:30:21 +0200

    add part 2 and bonus

--- quizGame/main.go
+++ quizGame/main.go
@@ -5,12 +5,16 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-	filePtr := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	filePtr := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer' (default problems.csv)")
+	timerPtr := flag.Int("t", 30, "the time limit for the quiz in seconds (default 30)")
+	shufflePtr := flag.Bool("shuffle", false, "shuffle questions or not (default false)")
 	flag.Parse()
 	file, err := os.Open(*filePtr)
 	if err != nil {
@@ -19,15 +23,36 @@ func main() {
 	}
 	fileReader := csv.NewReader(file)
 	records, _ := fileReader.ReadAll()
+	// shuffle
+	if *shufflePtr {
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(records), func(i, j int) { records[i], records[j] = records[j], records[i] })
+	}
 	cmdReader := bufio.NewReader(os.Stdin)
+	answers := make(chan (string))
 	score := 0
+	timer := time.NewTimer(time.Duration(*timerPtr) * time.Second)
+quizloop:
 	for _, r := range records {
 		fmt.Printf("%s: ", r[0])
-		text, _ := cmdReader.ReadString('\n')
-		t := strings.Trim(text, "\n")
-		if t == r[1] {
-			score++
+		go func() {
+			text, _ := cmdReader.ReadString('\n')
+			t := strings.Trim(text, "\n")
+			answers <- t
+		}()
+
+		select {
+		case <-timer.C:
+			break quizloop
+		case c := <-answers:
+			c = strings.TrimSpace(c)
+			c = strings.ToLower(c)
+			r[1] = strings.ToLower(r[1])
+			if c == r[1] {
+				score++
+			}
 		}
+
 	}
-	fmt.Printf("You scored %d out of %d.\n", score, len(records))
+	fmt.Printf("\nYou scored %d out of %d.\n", score, len(records))
 }
